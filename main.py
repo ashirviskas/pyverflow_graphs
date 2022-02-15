289299c4683ff8364ef12511dced561802e92d91
IWIllPull <minematas@gmail.com>
2022-02-15 23:49:06 +0200

    Bit better and more readable parallel graph

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from pandas.plotting import parallel_coordinates
 import pandas as pd
+import matplotlib.ticker as mtick
 
 
 def transform_data_year_only(data, column_name, year):
@@ -21,7 +22,6 @@ def merge_rows(data, rowname_a, rowname_b):
 
 
 def main():
-
     data_2015 = pd.read_csv('2015_loved_dreaded.csv')
     data_2015 = transform_data_year_only(data_2015, 'loved', 2015)
     data_2016 = pd.read_csv('2016_loved_dreaded.csv')
@@ -37,7 +37,6 @@ def main():
     data_2021 = pd.read_csv('2021_loved_dreaded.csv')
     data_2021 = transform_data_year_only(data_2021, 'loved', 2021)
 
-
     # Join the dataframes
     data = pd.merge(data_2015, data_2016, how='outer', left_index=True, right_index=True)
     data = pd.merge(data, data_2017, how='outer', left_index=True, right_index=True)
@@ -48,10 +47,15 @@ def main():
 
     # Plot the data
     data = merge_rows(data, 'Bash/Shell', 'Bash/Shell/PowerShell')
-    data = data.dropna(thresh=data.shape[1]*0.3,how='all',axis=0)
+    data = data.dropna(thresh=data.shape[1] * 0.3, how='all', axis=0)
+    print(f'Data points: {len(data)}')
 
     data.reset_index(level=0, inplace=True)
-    parallel_coordinates(data, 'Technology', colormap=plt.get_cmap('hsv'))
+    data = data.sort_values(by=[str(i) for i in range(2021, 2015, -1)], ascending=False)
+    ax = parallel_coordinates(data, 'Technology', colormap=plt.get_cmap('gist_ncar'), linewidth=3, alpha=0.8)
+
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+
     plt.show()
 
 
